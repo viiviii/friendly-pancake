5a9462ba7f36217b8e87e1900a8540c2e6bc4454
viiviii <viiiix.viiviii@gmail.com>
2023-04-09 19:55:46 +0900

    :recycle: refactor: 서비스 테스트를 개선한다
    - 예를 들어,
    - [시청하지 않은 컨텐츠 목록]일 때 url, title로 검증하는 것보다
    - 모두 시청되지 않은 상태인지를 검증하고 이 내용을 테스트에 담는 것이 더 좋다고 판단함

--- src/test/java/com/pancake/api/content/application/ContentServiceTest.java
+++ src/test/java/com/pancake/api/content/application/ContentServiceTest.java
@@ -1,20 +1,30 @@
 package com.pancake.api.content.application;
 
+import com.pancake.api.content.NetflixConstant;
 import com.pancake.api.content.application.dto.ContentRequest;
 import com.pancake.api.content.domain.Content;
 import com.pancake.api.content.infra.MemoryContentRepository;
+import org.assertj.core.api.Condition;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.pancake.api.content.NetflixConstant.PONYO;
 import static com.pancake.api.content.NetflixConstant.TOTORO;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.data.Index.atIndex;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ContentServiceTest {
 
     private final ContentService contentService = new ContentService(new MemoryContentRepository());
 
+    private final Condition<Content> watchedContent = new Condition<>(Content::isWatched, "content is watched");
+    private final Condition<Content> unwatchedContent = not(watchedContent);
+
+    private final long NOT_EXISTS_ID = Long.MAX_VALUE;
+
+
+    @DisplayName("컨텐츠를 저장한다")
     @Test
     void save() {
         //given
@@ -25,67 +35,16 @@ class ContentServiceTest {
 
         //then
         assertAll(
-                () -> assertThat(actual.id()).isPositive(),
                 () -> assertThat(actual.url()).isEqualTo(TOTORO.URL),
                 () -> assertThat(actual.title()).isEqualTo(TOTORO.TITLE)
         );
     }
 
-    @Test
-    void getAllContents() {
-        //given
-        savedContent(TOTORO.URL, TOTORO.TITLE);
-        savedContent(PONYO.URL, PONYO.TITLE);
-
-        //when
-        var contents = contentService.getAllContents();
-
-        //then
-        assertThat(contents).extracting(Content::url, Content::title)
-                .containsExactly(
-                        tuple(TOTORO.URL, TOTORO.TITLE), // TODO
-                        tuple(PONYO.URL, PONYO.TITLE) // TODO
-                );
-    }
-
-    @Test
-    void getUnwatchedContents() {
-        //given
-        savedContent(TOTORO.URL, TOTORO.TITLE);
-        savedContent(PONYO.URL, PONYO.TITLE);
-
-        //when
-        var contents = contentService.getUnwatchedContents();
-
-        //then
-        assertThat(contents).extracting(Content::url, Content::title)
-                .containsExactly(
-                        tuple(TOTORO.URL, TOTORO.TITLE),
-                        tuple(PONYO.URL, PONYO.TITLE)
-                );
-    }
-
-    @Test
-    void getWatchedContents() {
-        //given
-        savedContent(TOTORO.URL, TOTORO.TITLE).watch();
-        savedContent(PONYO.URL, PONYO.TITLE).watch();
-
-        //when
-        var contents = contentService.getWatchedContents();
-
-        //then
-        assertThat(contents).extracting(Content::url, Content::title)
-                .containsExactly(
-                        tuple(TOTORO.URL, TOTORO.TITLE),
-                        tuple(PONYO.URL, PONYO.TITLE)
-                );
-    }
-
+    @DisplayName("컨텐츠를 시청 처리한다")
     @Test
     void watch() {
         //given
-        var contentId = savedContent(TOTORO.URL, TOTORO.TITLE).id();
+        var contentId = existUnwatchedContent().id();
 
         //when
         var watched = contentService.watch(contentId);
@@ -94,8 +53,67 @@ class ContentServiceTest {
         assertThat(watched).isTrue();
     }
 
-    private Content savedContent(String url, String title) {
-        return contentService.save(new ContentRequest(url, title));
+    @DisplayName("존재하지 않는 컨텐츠를 시청 처리 시 예외가 발생한다")
+    @Test
+    void watchThrownExceptionWhenContentNotExist() {
+        assertThatThrownBy(() -> contentService.watch(NOT_EXISTS_ID))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("컨텐츠를 모두 조회한다")
+    @Test
+    void getAllContents() {
+        //given
+        existWatchedContent();
+        existUnwatchedContent();
+
+        //when
+        var actual = contentService.getAllContents();
+
+        //then
+        assertThat(actual)
+                .hasSize(2)
+                .has(watchedContent, atIndex(0))
+                .has(unwatchedContent, atIndex(1));
+    }
+
+    @DisplayName("시청하지 않은 컨텐츠를 모두 조회한다")
+    @Test
+    void getUnwatchedContents() {
+        //given
+        existWatchedContent();
+        existUnwatchedContent();
+
+        //when
+        var actual = contentService.getUnwatchedContents();
+
+        //then
+        assertThat(actual).hasSize(1).are(unwatchedContent);
+    }
+
+    @DisplayName("시청한 컨텐츠를 모두 조회한다")
+    @Test
+    void getWatchedContents() {
+        //given
+        existWatchedContent();
+        existUnwatchedContent();
+
+        //when
+        var actual = contentService.getWatchedContents();
+
+        //then
+        assertThat(actual).hasSize(1).are(watchedContent);
+    }
+
+    private void existWatchedContent() {
+        saveContent(TOTORO).watch();
+    }
+
+    private Content existUnwatchedContent() {
+        return saveContent(PONYO);
+    }
+
+    private Content saveContent(NetflixConstant netflix) {
+        return contentService.save(new ContentRequest(netflix.URL, netflix.TITLE));
+    }
 }
