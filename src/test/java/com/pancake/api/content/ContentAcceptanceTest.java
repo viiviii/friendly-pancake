c7978406d8f2b618089d32cce169c5bda3f5163b
viiviii <viiiix.viiviii@gmail.com>
2023-07-20 11:37:11 +0900

    🚚 move: test의 application.yml을 main으로 합친다 (#22)
    * :truck: move: test의 application.yml 설정을 main으로 이동한다
    
    * :fire: remove: 프로젝트 생성 시 기본으로 만들어졌던 테스트를 제거한다
    
    * :white_check_mark: test: 인수 테스트에 test profile을 명시한다

--- src/test/java/com/pancake/api/content/ContentAcceptanceTest.java
+++ src/test/java/com/pancake/api/content/ContentAcceptanceTest.java
@@ -5,6 +5,7 @@ import com.pancake.api.content.application.dto.ContentResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -17,6 +18,7 @@ import static org.assertj.core.groups.Tuple.tuple;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Sql("/database_cleanup.sql")
 @SuppressWarnings("NonAsciiCharacters")
