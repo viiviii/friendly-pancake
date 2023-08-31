e3ca688baa3ce327bdf90b3d0a49ab8c7f50fe80
viiviii <viiiix.viiviii@gmail.com>
2023-08-31 21:21:04 +0900

    :sparkles: feat: Url 클래스의 url() 기능을 구현한다

--- app/test/api/url_test.dart
+++ app/test/api/url_test.dart
@@ -2,15 +2,55 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pancake_app/api/api.dart' as api;
 import 'package:pancake_app/api/api.dart';
 
+class Url {
+  final Uri _url;
+
+  Url(Domain domain)
+      : _url = Uri.parse(domain.fromEnvironment()).resolve('api/');
+
+  Uri call() => _url;
+}
+
+class StubDomain implements Domain {
+  final String _domain;
+
+  StubDomain(this._domain);
+
+  @override
+  String fromEnvironment() => _domain;
+}
+
 void main() {
   test('Domain defaultValue', () {
     final domain = Domain();
     expect(domain.fromEnvironment(), 'http://localhost:8080');
   });
 
-  test('url()', () {
+  test('url(String path) ', () {
     expect('${api.url('contents')}', 'http://localhost:8080/api/contents');
     expect('${api.url('users')}', 'http://localhost:8080/api/users');
+  });
+
+  test('url() with http', () {
+    //given
+    final url = Url(StubDomain('http://www.localhost:81'));
+
+    //when
+    final actual = url();
+
+    //then
+    expect(actual, Uri.http('www.localhost:81', 'api/'));
+  });
+
+  test('url() with https', () {
+    //given
+    final url = Url(StubDomain('https://my.example.site:8443'));
+
+    //when
+    final actual = url();
+
+    //then
+    expect(actual, Uri.https('my.example.site:8443', 'api/'));
   });
 
   group('Uri 학습 테스트', () {
