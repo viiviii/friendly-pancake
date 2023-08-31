6477c2aef1a2c20d874deb6bf74c9e251daea0f7
viiviii <viiiix.viiviii@gmail.com>
2023-08-31 21:30:50 +0900

    :sparkles: feat: Url 클래스의 path() 기능을 구현한다

--- app/test/api/url_test.dart
+++ app/test/api/url_test.dart
@@ -9,6 +9,8 @@ class Url {
       : _url = Uri.parse(domain.fromEnvironment()).resolve('api/');
 
   Uri call() => _url;
+
+  Uri path(String path) => _url.resolve(path);
 }
 
 class StubDomain implements Domain {
@@ -24,6 +26,37 @@ void main() {
   test('Domain defaultValue', () {
     final domain = Domain();
     expect(domain.fromEnvironment(), 'http://localhost:8080');
+  });
+
+  test('url path http', () {
+    //given
+    final url = Url(StubDomain('http://localhost:8080'));
+
+    //when
+    final actual = url.path('posts');
+
+    //then
+    expect(actual, Uri.http('localhost:8080', 'api/posts'));
+  });
+
+  test('url path https', () {
+    //given
+    final url = Url(StubDomain('https://my.example.site:443'));
+
+    //when
+    final actual = url.path('posts');
+
+    //then
+    expect(actual, Uri.https('my.example.site:443', 'api/posts'));
+  });
+
+  test('path()를 여러 번 호출해도 /api/ 뒤의 마지막 {path}만 변경 된다', () {
+    //given
+    final url = Url(StubDomain('http://localhost:8080'));
+
+    //then
+    expect('${url.path('posts')}', 'http://localhost:8080/api/posts');
+    expect('${url.path('users')}', 'http://localhost:8080/api/users');
   });
 
   test('url(String path) ', () {
