953e4384cb1b229d0a32e3f3a555500cd9713e5d
viiviii <viiiix.viiviii@gmail.com>
2023-09-07 19:16:16 +0900

    :speech_balloon: chore: 응답 DTO에 빠져있던 url을 추가한다

--- api/src/main/java/com/pancake/api/content/application/dto/ContentResponse.java
+++ api/src/main/java/com/pancake/api/content/application/dto/ContentResponse.java
@@ -13,11 +13,12 @@ public class ContentResponse {
     private Long id;
     private String title;
     private String description;
+    private String url;
     private String imageUrl;
     private boolean watched;
 
     public static ContentResponse fromEntity(Content content) {
         return new ContentResponse(content.id(), content.title(), content.description(),
-                content.imageUrl(), content.isWatched());
+                content.url(), content.imageUrl(), content.isWatched());
     }
 }
