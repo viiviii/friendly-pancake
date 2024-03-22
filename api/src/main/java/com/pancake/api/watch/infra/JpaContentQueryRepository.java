0b0053a383b2200bc92cdb5c48c1bfcf86c402ff
viiviii <viiiix.viiviii@gmail.com>
2024-03-22 18:17:02 +0900

    :fire: remove: 불필요해진 코드를 제거한다

--- api/src/main/java/com/pancake/api/watch/infra/JpaContentQueryRepository.java
+++ api/src/main/java/com/pancake/api/watch/infra/JpaContentQueryRepository.java
@@ -5,12 +5,9 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 interface JpaContentQueryRepository extends Repository<Content, Long> {
 
     @EntityGraph(attributePaths = {"playbacks"})
     List<Content> findAll();
-
-    Optional<Content> findById(long id);
 }
