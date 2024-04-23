208e302dc2fe629ececde6bb6c87a4f24fc1cddf
viiviii <viiiix.viiviii@gmail.com>
2024-04-23 17:22:35 +0900

    :recycle: Clock을 주입 받아 시간을 테스트할 수 있게 한다

--- api/src/main/java/com/pancake/api/watch/application/GetContentsToWatch.java
+++ api/src/main/java/com/pancake/api/watch/application/GetContentsToWatch.java
@@ -3,9 +3,12 @@ package com.pancake.api.watch.application;
 import com.pancake.api.watch.domain.FindEnabledPlatforms;
 import com.pancake.api.watch.domain.FindWatchContent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
+import java.time.Clock;
+
+import static java.time.Instant.now;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +18,19 @@ public class GetContentsToWatch {
 
     private final FindEnabledPlatforms settings;
 
+    private final Clock clock;
+
     public Catalog query() {
-        final var enabledPlatforms = settings.findEnabledPlatformsAt(Instant.now());
+        final var enabledPlatforms = settings.findEnabledPlatformsAt(now(clock));
         final var watchableContents = contents.findAll().stream()
                 .filter(e -> e.canWatchOnAny(enabledPlatforms))
                 .toList();
 
         return new Catalog(watchableContents);
+    }
+
+    @Bean
+    private static Clock clock() {
+        return Clock.systemUTC();
     }
 }
--- api/src/test/java/com/pancake/api/watch/application/GetContentsToWatchTest.java
+++ api/src/test/java/com/pancake/api/watch/application/GetContentsToWatchTest.java
@@ -5,26 +5,41 @@ import com.pancake.api.watch.domain.FindWatchContent;
 import com.pancake.api.watch.domain.WatchContent;
 import org.junit.jupiter.api.Test;
 
+import java.time.Clock;
+
 import static com.pancake.api.content.domain.Platform.NETFLIX;
+import static java.time.Instant.parse;
+import static java.time.ZoneOffset.UTC;
 import static java.util.List.of;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 
 @SuppressWarnings("NonAsciiCharacters")
 class GetContentsToWatchTest {
 
     private final FindWatchContent findWatchContent = mock(FindWatchContent.class);
-    private final FindEnabledPlatforms findWatchSetting = mock(FindEnabledPlatforms.class);
-    private final GetContentsToWatch getContentsToWatch = new GetContentsToWatch(findWatchContent, findWatchSetting);
+    private final FindEnabledPlatforms findEnabledPlatforms = mock(FindEnabledPlatforms.class);
+    private final Clock clock = Clock.fixed(parse("2000-01-01T00:00:00Z"), UTC);
+    private final GetContentsToWatch getContentsToWatch = new GetContentsToWatch(findWatchContent, findEnabledPlatforms, clock);
+
+    @Test
+    void 현재_시간을_기준으로_활성화된_플랫폼을_찾는다() {
+        //when
+        getContentsToWatch.query();
+
+        //then
+        then(findEnabledPlatforms).should().findEnabledPlatformsAt(clock.instant());
+    }
 
     @Test
     void 설정한_플랫폼에서_시청_가능한_컨텐츠만_포함된다() {
         //given
         var content = mock(WatchContent.class);
+        given(findEnabledPlatforms.findEnabledPlatformsAt(any())).willReturn(of(NETFLIX));
         given(findWatchContent.findAll()).willReturn(of(content));
-        given(findWatchSetting.findEnabledPlatformsAt(any())).willReturn(of(NETFLIX));
         given(content.canWatchOnAny(of(NETFLIX))).willReturn(true);
 
         //when
@@ -38,8 +53,8 @@ class GetContentsToWatchTest {
     void 설정한_플랫폼에서_시청_불가능한_컨텐츠는_제외된다() {
         //given
         var content = mock(WatchContent.class);
-        given(findWatchContent.findAll()).willReturn(of(content, content, content));
-        given(findWatchSetting.findEnabledPlatformsAt(any())).willReturn(of(NETFLIX));
+        given(findEnabledPlatforms.findEnabledPlatformsAt(any())).willReturn(of(NETFLIX));
+        given(findWatchContent.findAll()).willReturn(of(content));
         given(content.canWatchOnAny(of(NETFLIX))).willReturn(false);
 
         //when
