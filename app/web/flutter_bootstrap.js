732e53a592e2f17f2e59ffe959020cd22a5615c0
viiviii <viiiix.viiviii@gmail.com>
2024-06-13 20:33:22 +0900

    :wrench: 이모지가 컬러로 나오도록 설정을 추가한다

--- app/web/flutter_bootstrap.js
+++ app/web/flutter_bootstrap.js
@@ -1,4 +1,11 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-_flutter.loader.load();
+_flutter.loader.load({
+  onEntrypointLoaded: async function(engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine({
+      useColorEmoji: true, // Add emoji color setting
+    });
+    await appRunner.runApp();
+  }
+});
