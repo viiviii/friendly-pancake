736a9d30cef36b1086991d23f38f4a8683980522
viiviii <viiiix.viiviii@gmail.com>
2024-07-12 16:15:50 +0900

    :white_check_mark: 북마크 저장 요청 데이터를 검증하는 테스트를 추가한다

---
+++ api/src/test/java/com/pancake/api/bookmark/BookmarkServiceTest.java
@@ -0,0 +1,40 @@
+package com.pancake.api.bookmark;
+
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.Test;
+
+import static com.pancake.api.bookmark.Builders.aBookmarkSaveCommand;
+import static com.pancake.api.content.Builders.aMetadata;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+@SuppressWarnings("NonAsciiCharacters")
+class BookmarkServiceTest {
+
+    private final GetContentMetadata getContentMetadata = mock(GetContentMetadata.class);
+    private final BookmarkRepository bookmarkRepository = mock(BookmarkRepository.class);
+    private final BookmarkService bookmarkService = new BookmarkService(getContentMetadata, bookmarkRepository);
+
+
+    @Test
+    void 저장한_제목과_검색결과의_제목이_다른_경우_예외가_발생한다() {
+        //given
+        var bookmark = aBookmarkSaveCommand()
+                .title("저장할 제목")
+                .contentId("8392")
+                .contentType("movie")
+                .contentSource("TMDB")
+                .build();
+
+        given(getContentMetadata.queryBy("8392"))
+                .willReturn(aMetadata().title("다른 제목").build());
+
+
+        //when
+        ThrowingCallable actual = () -> bookmarkService.save(bookmark);
+
+        //then
+        assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
+    }
+}
