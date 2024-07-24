6b4b716f7a626cd900f30ef694211fc4f0f3b1f5
viiviii <viiiix.viiviii@gmail.com>
2024-07-24 18:28:36 +0900

    :sparkles: 화면 크기에 따라 메뉴 위치가 달라지도록 구현한다

--- app/lib/content/content_screen.dart
+++ app/lib/content/content_screen.dart
@@ -3,6 +3,7 @@ import 'package:pancake_app/api/api.dart' show api;
 import 'package:pancake_app/content/bookmark/bookmark_screen.dart';
 import 'package:pancake_app/content/search/content_search_screen.dart';
 import 'package:pancake_app/content/search/view_models/content_search_view_model.dart';
+import 'package:pancake_app/widgets/overflow_navigation_bar.dart';
 
 class ContentScreen extends StatefulWidget {
   const ContentScreen({super.key});
@@ -12,16 +13,16 @@ class ContentScreen extends StatefulWidget {
 }
 
 class _ContentScreenState extends State<ContentScreen> {
-  late final List<Destination> _menu = [
-    Destination(
-      screen: ContentSearchScreen(onSelected: _addToBookmark),
+  late final List<_Menu> _menu = [
+    _Menu(
+      ContentSearchScreen(onSelected: _onSearchContentSelected),
       icon: const Icon(Icons.search),
-      label: '검색',
+      label: const Text('검색'),
     ),
-    const Destination(
-      screen: BookmarkScreen(),
+    const _Menu(
+      BookmarkScreen(),
       icon: Icon(Icons.bookmark),
-      label: '북마크',
+      label: Text('북마크'),
     ),
   ];
 
@@ -29,14 +30,25 @@ class _ContentScreenState extends State<ContentScreen> {
 
   Future<void> _onMenuSelected(int index) async {
     setState(() => _currentIndex = index);
+    _clearSavedMessages();
+  }
+
+  Future<void> _onSearchContentSelected(SearchContent searchContent) async {
+    await _addToBookmark(searchContent);
+    _alertSavedMessage();
+  }
+
+  void _alertSavedMessage() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('저장 되었습니다.')),
+    );
+  }
+
+  void _clearSavedMessages() {
+    ScaffoldMessenger.of(context).clearSnackBars();
   }
 
   Future<void> _addToBookmark(SearchContent searchContent) async {
-    await _postBookmark(searchContent);
-    alertSavedMessage();
-  }
-
-  Future<void> _postBookmark(SearchContent searchContent) async {
     await api.post('bookmarks', body: {
       'contentSource': 'TMDB', // TODO: 하드코딩
       'contentId': searchContent.id,
@@ -45,90 +57,30 @@ class _ContentScreenState extends State<ContentScreen> {
     });
   }
 
-  void alertSavedMessage() {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('저장 되었습니다.')),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    print('screenWidth=$screenWidth');
     return Scaffold(
       appBar: AppBar(
         title: const Text('컨텐츠'),
       ),
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (screenWidth > 700)
-            _RailNavigation(
-              onDestinationSelected: _onMenuSelected,
-              destinations: _menu
-                  .map((e) => NavigationRailDestination(
-                      icon: e.icon, label: Text(e.label)))
-                  .toList(),
-              selectedIndex: _currentIndex,
-            ),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 30,
-                horizontal: 100,
-              ),
-              child: _menu[_currentIndex].screen,
-            ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 15),
+        child: OverflowNavigationBar(
+          onDestinationSelected: _onMenuSelected,
+          destinations: _menu,
+          selectedIndex: _currentIndex,
+          body: Padding(
+            padding: const EdgeInsets.all(30),
+            child: _menu[_currentIndex].screen,
           ),
-        ],
+        ),
       ),
-      bottomNavigationBar: screenWidth <= 700
-          ? NavigationBar(
-              // labelBehavior: labelBehavior,
-              selectedIndex: _currentIndex,
-              onDestinationSelected: _onMenuSelected,
-              destinations: _menu
-                  .map((e) =>
-                      NavigationDestination(icon: e.icon, label: e.label))
-                  .toList(),
-            )
-          : null,
     );
   }
 }
 
-class _RailNavigation extends StatelessWidget {
-  const _RailNavigation({
-    required this.onDestinationSelected,
-    required this.destinations,
-    required this.selectedIndex,
-  });
-
-  final ValueChanged<int> onDestinationSelected;
-  final List<NavigationRailDestination> destinations;
-  final int selectedIndex;
-
-  @override
-  Widget build(BuildContext context) {
-    return NavigationRail(
-      selectedIndex: selectedIndex,
-      onDestinationSelected: onDestinationSelected,
-      destinations: destinations,
-      extended: true,
-      minExtendedWidth: 150,
-    );
-  }
-}
-
-class Destination {
-  const Destination({
-    required this.screen,
-    required this.icon,
-    required this.label,
-  });
+class _Menu extends NavigationRailDestination {
+  const _Menu(this.screen, {required super.icon, required super.label});
 
   final Widget screen;
-  final Widget icon;
-  final String label;
 }
